9d86c728483d1e539cc11cd85af319c77ac56d04
xlxlzh <382838217@qq.com>
2019-12-09 22:55:34 +0800

    Begin Vector2.

---
+++ Ghost/Engine/include/Vector2.h
@@ -0,0 +1,35 @@
+#ifndef _VECTOR2_H_
+#define _VECTOR2_H_
+
+namespace ghost
+{
+	template<typename T>
+	class Vector2
+	{
+	public:
+		Vector2() : _x(0), _y(0) { }
+		Vector2(T x, T y) : _x(x), _y(y) { }
+		Vector2(T rhs) : _x(rhs), _y(rhs) { }
+		Vector2(const T* arr) : _x(arr[0]), _y(arr[1]) { }
+		Vector2(T const* arr) : _x(arr[0]), _y(arr[1]) { }
+
+		Vector2(const Vector2<T>& rhs) : _x(rhs._x), _y(rhs._y) { }
+
+		Vector2<T>& operator= (const Vector2<T>& rhs) { _x = rhs._x; _y = rhs._y; return *this; }
+
+		Vector2<T> operator+ (const Vector2<T>& rhs) const { return Vector2(_x + rhs._x, _y + rhs._y); }
+		Vector2<T> operator- (const Vector2<T>& rhs) const { return Vector2(_x - rhs._x, _y - rhs._y); }
+		Vector2<T> operator* (const Vector2<T>& rhs) const { return Vector2(_x * rhs._x, _y * rhs._y); }
+
+		Vector2<T>& operator+= (const Vector2<T>& rhs) { _x += rhs._x; _y += rhs._y; return *this; }
+		Vector2<T>& operator-= (const Vector2<T>& rhs) { _x -= rhs._x; _y -= rhs._y; return *this; }
+		Vector2<T>& operator*= (const Vector2<T>& rhs) { _x *= rhs._x; _y *= rhs._y; return *this; }
+
+	public:
+		T _x, _y;
+	};
+
+	using Vector2f = Vector2<float>;
+}
+
+#endif
