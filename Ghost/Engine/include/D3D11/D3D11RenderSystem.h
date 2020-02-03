dce8c45ccc346d94b1aa18d1a746ff5189e4cc53
xlxlzh <xuleiselfmail@gmail.com>
2020-02-03 21:19:46 +0800

    Add some interface.

--- Ghost/Engine/include/D3D11/D3D11RenderSystem.h
+++ Ghost/Engine/include/D3D11/D3D11RenderSystem.h
@@ -1,12 +1,30 @@
 #ifndef _D3D11_RENDERSYSTEM_H_
 #define _D3D11_RENDERSYSTEM_H_
 
+#include <d3d11.h>
 #include "RenderSystem.h"
 
 namespace ghost
 {
     class D3D11RenderSystem : public RenderSystem
     {
+    public:
+        virtual bool initRendersystem() override;
+
+        virtual void setRenderTarget() override;
+        virtual void setRenderTargets() override;
+        virtual void setDepthstencil() override;
+        virtual void setClearColor() override;
+        virtual void clearRenderTarget() override;
+        virtual void clearRenderTargets() override;
+
+        virtual void drawPrimitive() override;
+        virtual void drawPrimitiveIndexed() override;
+        virtual void drawPrimitiveInstance() override;
+
+        virtual void endScene() override;
+
+    private:
 
     };
 }
--- Ghost/Engine/include/RenderSystem.h
+++ Ghost/Engine/include/RenderSystem.h
@@ -5,7 +5,23 @@ namespace ghost
 {
     class RenderSystem
     {
+    public:
+        virtual bool initRendersystem() = 0;
 
+        virtual void setRenderTarget() = 0;
+        virtual void setRenderTargets() = 0;
+        virtual void setDepthstencil() = 0;
+        virtual void setClearColor() = 0;
+        virtual void clearRenderTarget() = 0;
+        virtual void clearRenderTargets() = 0;
+
+        virtual void drawPrimitive() = 0;
+        virtual void drawPrimitiveIndexed() = 0;
+        virtual void drawPrimitiveInstance() = 0;
+
+        virtual void endScene() = 0;
+
+    protected:
     };
 }
 
