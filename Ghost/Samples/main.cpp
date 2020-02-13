bcda1a910613bad04df5ff1cd5306a98e45c5eea
xlxlzh <xuleiselfmail@gmail.com>
2020-02-13 21:59:41 +0800

    Delete log test code.

--- Ghost/Samples/main.cpp
+++ Ghost/Samples/main.cpp
@@ -7,12 +7,6 @@ using namespace ghost;
 
 INT WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd )
 {
-    LogManager::getInstance()->addLog("debug.txt");
-    LogManager::getInstance()->logMessage(LOG_DEBUG, "debug info");
-    LogManager::getInstance()->logMessage(LOG_ERROR, "error info");
-    LogManager::getInstance()->logMessage(LOG_INFO, "info info");
-    LogManager::getInstance()->logMessage(LOG_WARNING, "warning info");
-
     Engine::getInstance()->initEngine(RENDER_D3D11);
 
     Application* app = new Application();
