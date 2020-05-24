d33f318d62c52d2388510d6941566b987f9e65b2
xlxlzh <xuleiselfmail@gmail.com>
2020-05-24 20:21:02 +0800

    Resource first commit.

---
+++ Ghost/Engine/include/Texture2D.h
@@ -0,0 +1,13 @@
+#include "Resource.h"
+
+namespace ghost
+{
+    class Texture2D : public Resource
+    {
+    public:
+
+    protected:
+        unsigned _width;
+        unsigned _height;
+    };
+}
