debd1d4bdcd6f1188c0ceb3160affb591d9ebc2a
xlxlzh <382838217@qq.com>
2020-08-17 12:19:49 +0800

    Change sample project dir.

--- Ghost/Samples/SampleApplication/SampleApplication.cpp
+++ Ghost/Samples/SampleApplication/SampleApplication.cpp
@@ -1,11 +1,17 @@
 #include "SampleApplication.h"
 #include "Engine.h"
+#include <windows.h>
 
 void SampleApplication::onInit()
 {
     Engine::getInstance()->initEngine(getAttachWindow(), RENDER_D3D11, _4x, _window->getWidth(), _window->getHeight());
     auto ri = Engine::getInstance()->getRenderSystem();
     ri->setClearColor(Color::Blue);
+
+    auto fileSystem = Engine::getInstance()->getFileSystem();
+    fileSystem->setCurrentDir("../Resources");
+    //MessageBoxA(nullptr, fileSystem->getCurrentDir().c_str(), __FUNCTION__, MB_OK);
+    //ResourceManager::getInstance()->addResource(RESOURCE_MATERIAL, "light.material.xml", 0);
 }
 
 void SampleApplication::onExit()
