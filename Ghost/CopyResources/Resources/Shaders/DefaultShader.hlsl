a5b4dd7fc80e64eed5073b2ebbaa8848dbdb40c3
xlxlzh <382838217@qq.com>
2020-09-08 18:13:20 +0800

    Update DefaultShader.hlsl to test.

--- Ghost/CopyResources/Resources/Shaders/DefaultShader.hlsl
+++ Ghost/CopyResources/Resources/Shaders/DefaultShader.hlsl
@@ -1,4 +1,24 @@
-float4 vs_main() : SV_POSITION
+uniform float4x4 matWorld;
+uniform float4x4 matWorldViewProj;
+
+cbuffer UpdatePerFrame
 {
-    return float4(1.0, 1.0, 0.0, 1.0);
+    float4 abcd;
+};
+
+Texture2D texAlbedo;
+Texture2D texNormal;
+
+SamplerState samplerNormal;
+
+struct vs_input
+{
+    float4 position : POSITION0;
+    float2 uv       : TEXCOORD0;
+    float3 normal   : NORMAL;
+};
+
+float4 vs_main(vs_input input) : SV_POSITION
+{
+    return mul(input.position, matWorld);
 }
