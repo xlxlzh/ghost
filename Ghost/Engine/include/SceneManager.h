930550840495efcdbf74f51f2310d25d9d16a4b5
xlxlzh <382838217@qq.com>
2020-09-25 18:10:11 +0800

    Prepare for scenemanager.

---
+++ Ghost/Engine/include/SceneManager.h
@@ -0,0 +1,11 @@
+#ifndef _SCENEMANAGER_H_
+#define _SCENEMANAGER_H_
+
+#include "Ghost.h"
+
+namespace ghost
+{
+
+}
+
+#endif
---
+++ Ghost/Engine/include/SceneNode.h
@@ -0,0 +1,11 @@
+#ifndef _SCENENODE_H_
+#define _SCENENODE_H_
+
+#include "Ghost.h"
+
+namespace ghost
+{
+
+}
+
+#endif
