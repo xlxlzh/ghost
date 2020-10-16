2304b3ec485ee9682b08127e7fd5ad701d9cac66
xlxlzh <382838217@qq.com>
2020-10-16 19:32:19 +0800

    Add interface for BoundingBox

--- Ghost/Engine/include/BoundingBox.h
+++ Ghost/Engine/include/BoundingBox.h
@@ -14,6 +14,13 @@ namespace ghost
         BoundingBox(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
         ~BoundingBox() = default;
 
+        Vector3<float> getSize() const { return _max - _min; }
+        Vector3<float> getCenter() const;
+
+        Vector3<float> getHalfSize() const;
+
+        bool contains(const Vector3<float>& point) const;
+
     public:
         Vector3<float> _min;
         Vector3<float> _max;
--- Ghost/Engine/src/BoundingBox.cpp
+++ Ghost/Engine/src/BoundingBox.cpp
@@ -18,4 +18,21 @@ namespace ghost
         _min._y = minY;
         _min._z = minZ;
     }
+
+    Vector3<float> BoundingBox::getHalfSize() const
+    {
+        return (_max - _min) * 0.5;
+    }
+
+    Vector3<float> BoundingBox::getCenter() const
+    {
+        return (_max + _min) * 0.5;
+    }
+
+    bool BoundingBox::contains(const Vector3<float>& point) const
+    {
+        return point._x >= _min._x && point._y >= _min._y &&
+            point._z >= _min._z && point._x <= _max._x && point._y <= _max._y &&
+            point._z <= _max._z;
+    }
 }
