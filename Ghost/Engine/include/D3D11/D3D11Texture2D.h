cbb28fa91f6044d3830bcff8be1c406a0bd90db6
xlxlzh <382838217@qq.com>
2021-03-12 23:32:21 +0800

    D3D11 View

--- Ghost/Engine/include/D3D11/D3D11Texture2D.h
+++ Ghost/Engine/include/D3D11/D3D11Texture2D.h
@@ -12,8 +12,18 @@ namespace ghost
         D3D11Texture2D();
         ~D3D11Texture2D();
 
+        ID3D11Texture2DPtr getD3D11Texture2D() const { return _texture; }
+
+        ID3D11ShaderResourceViewPtr getD3D11ShaderResourceView() const { return _shaderResourceView };
+        ID3D11DepthStencilViewPtr getD3D11DepthStencilView() const { return _depthStencilView; }
+        ID3D11RenderTargetViewPtr getD3D11RenderTargetView() const { return _rendertargetView; }
+
     private:
         ID3D11Texture2DPtr _texture;
+
+        ID3D11ShaderResourceViewPtr _shaderResourceView;
+        ID3D11RenderTargetViewPtr _rendertargetView;
+        ID3D11DepthStencilViewPtr _depthStencilView;
     };
 
     DECLAR_SMART_POINTER(D3D11Texture2D)
