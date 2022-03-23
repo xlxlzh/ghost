cbea6298f6499689bf5ff3625f35d992ba6d77fb
xlxlzh <382838217@qq.com>
2022-03-23 23:19:55 +0800

    Fix a link error.

--- Ghost/Engine/src/Model.cpp
+++ Ghost/Engine/src/Model.cpp
@@ -2,6 +2,11 @@
 
 namespace ghost
 {
+    Model::Model(int type, const std::string& name, int flag) : Resource(type, name, flag)
+    {
+
+    }
+
     bool Model::load(DataStream& dataStream)
     {
         if (dataStream.getSize() <= 0)
