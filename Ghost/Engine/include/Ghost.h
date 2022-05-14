3d725317b938e6e4065e6f90673e3ee81c623d44
xlxlzh <382838217@qq.com>
2022-05-14 23:31:59 +0800

    Disable warning C4251  at windows

--- Ghost/Engine/include/Ghost.h
+++ Ghost/Engine/include/Ghost.h
@@ -15,6 +15,10 @@
 #endif
 #endif
 
+#ifdef WIN32
+#pragma warning(disable:4251)
+#endif
+
 #define DECLAR_SMART_POINTER(obj)  \
 using obj##Ptr = std::shared_ptr<obj>;  \
 using obj##WeakPtr = std::weak_ptr<obj>; \
