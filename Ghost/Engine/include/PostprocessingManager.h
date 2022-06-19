32eb6ef1dee3bb5e5ddf09e92446b82af0beaa49
xlxlzh <382838217@qq.com>
2022-06-19 23:33:11 +0800

    PostprocessingManager init

---
+++ Ghost/Engine/include/PostprocessingManager.h
@@ -0,0 +1,14 @@
+#ifndef _POSTPROCESSING_MANAGER_H_
+#define _POSTPROCESSING_MANAGER_H_
+
+#include "Ghost.h"
+
+namespace ghost
+{
+    class GHOST_API PostprocessingManager
+    {
+
+    };
+}
+
+#endif
