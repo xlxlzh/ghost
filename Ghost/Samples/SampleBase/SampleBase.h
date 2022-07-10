b1ab790e843a23530cb47888f644fe0df9bf9bd7
xlxlzh <382838217@qq.com>
2022-07-11 02:32:32 +0800

    SampleBase init

--- Ghost/Samples/SampleBase/SampleBase.h
+++ Ghost/Samples/SampleBase/SampleBase.h
@@ -7,5 +7,22 @@ using namespace ghost;
 
 class SampleBase : public Application
 {
+public:
+    virtual void onInit() override;
+    virtual void onExit() override;
+    virtual void onUpdate() override;
+    virtual void onTick(float deltaTime) override;
 
+    virtual void onKeydown(GhostKey key) override;
+
+private:
+    void _generateResourcesPath(const std::string& workdir);
+
+private:
+    std::string _resourcesPath{};
+
+    SceneManager* _scene;
+    Camera* _mainCamera;
+
+    Light* _mainLight;
 };
