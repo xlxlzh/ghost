67d60ebc793f570e12aa339902ade26ebee3bb5a
xlxlzh <382838217@qq.com>
2022-08-31 00:02:17 +0800

    D3D12ComPtr.h

--- Ghost/Engine/include/D3D12/D3D12ComPtr.h
+++ Ghost/Engine/include/D3D12/D3D12ComPtr.h
@@ -9,9 +9,11 @@ using namespace Microsoft::WRL;
 using ID3D12DevicePtr               = ComPtr<ID3D12Device>;
 using ID3D12CommandQueuePtr         = ComPtr<ID3D12CommandQueue>;
 using ID3D12CommandAllocatorPtr     = ComPtr<ID3D12CommandAllocator>;
+using ID3D12CommandListPtr          = ComPtr<ID3D12CommandList>;
 using ID3D12GraphicsCommandListPtr  = ComPtr<ID3D12GraphicsCommandList>;
 using ID3D12DescriptorHeapPtr       = ComPtr<ID3D12DescriptorHeap>;
 using ID3D12ResourcePtr             = ComPtr<ID3D12Resource>;
+using ID3D12FencePtr                = ComPtr<ID3D12Fence>;
 
 
 #endif // !_D3D11_COMPTR_H_
