2644389ff22a12fdd3da52f103fde4913d199f64
xlxlzh <382838217@qq.com>
2026-04-21 22:59:25 +0800

    [*] First version of thread pool

--- Ghost/Engine/include/ThreadPool.h
+++ Ghost/Engine/include/ThreadPool.h
@@ -1,4 +1,10 @@
+#include <future>
+#include <mutex>
 #include <thread>
+#include <condition_variable>
+#include <queue>
+#include <functional>
+#include <vector>
 #include "Ghost.h"
 
 namespace ghost
@@ -9,6 +15,43 @@ namespace ghost
         ThreadPool(int numThreads);
     
         ~ThreadPool();
-        // TODO
+        
+        template <typename F, typename... Args>
+        auto SubmitTask(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>>
+        {
+            using returnType = std::invoke_result_t<F, Args...>;
+            auto packedTask = std::make_shared<std::packaged_task<returnType()>>(
+                [func = std::forward<F>(f), ...args = std::forward<Args>(args)]() mutable
+                {
+                    return std::invoke(func, std::move(args)...);
+                });
+            
+            std::future<returnType> task = packedTask->get_future();
+            {
+                std::lock_guard<std::mutex> lock(_mutex);
+                
+                if (!_running)
+                {
+                    throw std::runtime_error("Can't submit task to a stopped thread pool.");
+                }
+                
+                _tasks.emplace([packedTask]()
+                {
+                    (*packedTask)();
+                });
+            }
+            
+            _cond.notify_one();
+            return task;
+        }
+        
+    private:
+        int _numThreads;
+        bool _running;
+        
+        std::vector<std::thread> _workers;
+        std::mutex _mutex;
+        std::condition_variable _cond;
+        std::queue<std::function<void()>> _tasks;
     };
 }
--- Ghost/Engine/src/ThreadPool.cpp
+++ Ghost/Engine/src/ThreadPool.cpp
@@ -2,13 +2,48 @@
 
 namespace ghost
 {
-    ThreadPool::ThreadPool(int numThreads)
+    ThreadPool::ThreadPool(int numThreads) : _numThreads(numThreads), _running(true)
     {
-        
+        for (int i = 0; i < numThreads; ++i)
+        {
+            _workers.emplace_back(
+                [this]()
+            {
+                while (true)
+                {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(_mutex);
+                        this->_cond.wait(lock, [this]
+                        {
+                            return !this->_running || !this->_tasks.empty();
+                        });
+                        
+                        if (!this->_running && this->_tasks.empty())
+                        {
+                            return;
+                        }
+                        
+                        task = std::move(this->_tasks.front());
+                        this->_tasks.pop();
+                    }
+                    task();
+                }
+            });
+        }
     }
 
     ThreadPool::~ThreadPool()
     {
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            _running = false;
+        }
         
+        _cond.notify_all();
+        for (std::thread& worker : _workers)
+        {
+            worker.join();
+        }
     }
 }
